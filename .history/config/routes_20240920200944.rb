0dd9fd816a1ebc54574381aed818e4d586fce687
Carlito Pedida Jr <carlped417@gmail.com>
2024-09-20 21:28:01 -0500

    logged in user restrictions

---
+++ .history/config/routes_20240920200944.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root "page#home"
+  get "about", to: "pages#about"
+
+  get "signup", to: "users#new"
+  resources :users, except: [ :new ]
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  resources :articles do
+    resources :comments
+  end
+end
